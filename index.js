cd5dd01ac2efc34e0e90bba38a09ca87c594fb13
deboker <117391017+deboker@users.noreply.github.com>
2023-08-08 08:17:44 +0200

    s

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function getCurrentTime() {
 setInterval(getCurrentTime, 1000)
 
 navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
         .then(res => {
             if (!res.ok) {
                 throw Error("Weather data not available")
